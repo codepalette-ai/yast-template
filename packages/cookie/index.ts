5283e5977ad2b6fd31b25333a051ea9046f472d5
vivek-codepalette <vivek@codepalette.ai>
2025-04-29 16:11:09 +0700

    Add Cookie

---
+++ packages/cookie/index.ts
@@ -0,0 +1,2 @@
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+export { default as CookieConsent } from "./cookie-consent";
--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,9 +1,10 @@
-import { AnalyticsProvider } from '@repo/analytics';
-import { AuthProvider } from '@repo/auth/provider';
-import type { ThemeProviderProps } from 'next-themes';
-import { Toaster } from './components/ui/sonner';
-import { TooltipProvider } from './components/ui/tooltip';
-import { ThemeProvider } from './providers/theme';
+import { AnalyticsProvider } from "@repo/analytics";
+import { AuthProvider } from "@repo/auth/provider";
+import type { ThemeProviderProps } from "next-themes";
+import { Toaster } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { ThemeProvider } from "./providers/theme";
+import { CookieConsent } from "@repo/cookie";
 
 type DesignSystemProviderProperties = ThemeProviderProps & {
   privacyUrl?: string;
@@ -23,6 +24,7 @@ export const DesignSystemProvider = ({
       <AnalyticsProvider>
         <TooltipProvider>{children}</TooltipProvider>
         <Toaster />
+        <CookieConsent />
       </AnalyticsProvider>
     </AuthProvider>
   </ThemeProvider>
