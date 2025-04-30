e1684191b945c3b55ccad071d62458c5a37da4ab
vivek-codepalette <vivek@codepalette.ai>
2025-04-30 14:05:23 +0700

    cleanup

--- apps/web/app/(landing-pages)/layout.tsx
+++ apps/web/app/(landing-pages)/layout.tsx
@@ -1,11 +1,5 @@
 import type { ReactNode } from "react";
-import { Header } from "@/components/header";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <Header />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
