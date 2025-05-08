832c6032e92f00bf4c2dba3a7331ac3dee51bb45
vivek-codepalette <vivek@codepalette.ai>
2025-05-08 14:41:09 +0700

    Updated Packages

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -3,13 +3,6 @@ import "server-only";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "./generated/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const database =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = database;
-}
+export const database = new PrismaClient().$extends(withAccelerate());
 
 export * from "./generated/client";
